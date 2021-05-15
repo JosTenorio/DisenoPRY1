3202bd76aaf60924ca494a423c59a59c81999124
Joseph <jostenoriop@gmail.com>
2021-05-15 12:19:09 -0600

    Finished database project and added ER model

--- SmrPrototype/CategoriaIng.sql
+++ SmrPrototype/CategoriaIng.sql
@@ -1,5 +1,8 @@
 ﻿CREATE TABLE [dbo].[CategoriaIng]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Nombre] NVARCHAR(50) NOT NULL
+    [Nombre] NVARCHAR(50) NOT NULL,
+	[IdCategoriaPadre] INT NULL, 
+    CONSTRAINT FkSubCategoriaIng FOREIGN KEY (IdCategoriaPadre) REFERENCES CategoriaIng (Id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    CONSTRAINT AkCategoriaIng UNIQUE(Nombre)
 )
--- SmrPrototype/Mesa.sql
+++ SmrPrototype/Mesa.sql
@@ -7,5 +7,7 @@
     [TamanoA] DECIMAL NOT NULL, 
     [TamanoB] DECIMAL NULL,
     [PosicionX] DECIMAL NOT NULL,
-    [PosicionY] DECIMAL NOT NULL, 
+    [PosicionY] DECIMAL NOT NULL,
+    CONSTRAINT AkMesa UNIQUE(Nombre),
+    CONSTRAINT ChMesa CHECK (TamanoA >= 0 AND ISNULL(TamanoB,0) >= 0)
 )
