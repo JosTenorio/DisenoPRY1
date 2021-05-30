d85e06df7619df30943469084b186457ce48fd07
Jose Pablo Munoz <jpmunoz@estudiantec.cr>
2021-05-30 00:41:21 -0600

    Added all 13 tables GUI

--- Restaurant POS/src/Controller/TablesController.java
+++ Restaurant POS/src/Controller/TablesController.java
@@ -5,30 +5,44 @@ import javafx.scene.input.MouseEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseButton;
 
 public class TablesController {
 
     @FXML
-    private Button table1;
-    @FXML
     private ImageView hambMenu;
     @FXML
     private ImageView edit;
+    @FXML
+    private Button table1;
+    @FXML
+    private Button table2;
+    @FXML
+    private Button table3;
+    @FXML
+    private Button table4;
+    @FXML
+    private Button table5;
+    @FXML
+    private Button table6;
+    @FXML
+    private Button table7;
+    @FXML
+    private Button table8;
+    @FXML
+    private Button table9;
+    @FXML
+    private Button table10;
+    @FXML
+    private Button table11;
+    @FXML
+    private Button table12;
+    @FXML
+    private Button table13;
 
     @FXML
     private void btnHandle(MouseEvent event) {
         if (event.getSource() == table1){
-            tableHandle(event);
-        }
-    }
-    
-    private void tableHandle(MouseEvent event){
-        if (event.getButton() == MouseButton.PRIMARY){
-            System.out.println("BTN 1");
-        }
-        else if (event.getButton() == MouseButton.SECONDARY){
-            System.out.println("BTN 2");
+            
         }
     }
 }
