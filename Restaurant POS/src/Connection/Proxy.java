b9689fc96f4caef3c9521415eeedfc33b7cd1df0
Joseph <jostenoriop@gmail.com>
2021-06-05 17:43:24 -0600

    Changed proxy path to relative

--- Restaurant POS/src/Connection/Proxy.java
+++ Restaurant POS/src/Connection/Proxy.java
@@ -12,8 +12,8 @@ import com.profesorfalken.jpowershell.*;
  * @author JOS
  */
 public class Proxy {
-    private static final String proxyDir = "C:\\Users\\JOS\\Desktop\\DisenoPRY1\\Restaurant POS";
-    private static final String proxyCommand = ".//cloud_sql_proxy -instances=hallowed-trail-313100:us-central1:sgr-prototype=tcp:1433";
+    private static final String proxyDirCommand = "sl C:\\Users\\JOS\\Desktop\\DisenoPRY1\\Restaurant POS";
+    private static final String proxyInitCommand = ".//cloud_sql_proxy -instances=hallowed-trail-313100:us-central1:sgr-prototype=tcp:1433";
     private static PowerShell PowerShellSession = null;
     
     
@@ -29,7 +29,8 @@ public class Proxy {
     
     public static void openProxy () {
        openPSSession();
-       PowerShellResponse response = PowerShellSession.executeCommand(".//cloud_sql_proxy -instances=hallowed-trail-313100:us-central1:sgr-prototype=tcp:1433");
+       PowerShellResponse response = PowerShellSession.executeCommand(proxyDirCommand);
+       response = PowerShellSession.executeCommand(proxyInitCommand);
     }
     
     
