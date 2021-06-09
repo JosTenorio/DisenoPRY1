399e0438bd49c73ffcc3cd89baf8554ae7e5d031
Jose Pablo Munoz <jpmunoz@estudiantec.cr>
2021-06-09 17:51:04 -0600

    Created objects order and item

---
+++ Restaurant POS/src/Model/Item.java
@@ -0,0 +1,12 @@
+
+package Model;
+
+import java.util.ArrayList;
+
+class Item {
+    
+    String name;
+    boolean isMainDish;
+    ArrayList<String> sideDishes;
+    String notes;
+}
---
+++ Restaurant POS/src/Model/Order.java
@@ -0,0 +1,10 @@
+
+package Model;
+
+import java.util.ArrayList;
+
+public class Order {
+    
+    String tableName;
+    ArrayList<Item> items;
+}
