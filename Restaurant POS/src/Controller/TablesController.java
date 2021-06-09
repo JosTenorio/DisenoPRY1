ced749cd3c4dbd6719ea2f81fa29c5e3264c30ca
Jose Pablo Munoz <jpmunoz@estudiantec.cr>
2021-06-09 12:54:32 -0600

    Added side bar

--- Restaurant POS/src/Controller/TablesController.java
+++ Restaurant POS/src/Controller/TablesController.java
@@ -2,15 +2,22 @@
 package Controller;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
+import javafx.animation.TranslateTransition;
 import javafx.scene.input.MouseEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
-public class TablesController extends SceneController {
+public class TablesController extends SceneController implements Initializable {
      
-    private ArrayList<String> tableIds;
+    private ArrayList<String> tableNames;
+    private boolean menuOpen;
 
     @FXML
     private ImageView hambMenu;
@@ -42,11 +49,37 @@ public class TablesController extends SceneController {
     private Button table12;
     @FXML
     private Button table13;
+    @FXML
+    private AnchorPane slider;
 
     @FXML
     private void btnHandle(MouseEvent event) throws IOException {
-        if (event.getSource() == table1){
-            tablesShow(event);
+        if (event.getSource() == hambMenu){
+            if (!menuOpen){
+                TranslateTransition slide = new TranslateTransition();
+                slide.setDuration(Duration.seconds(0.4));
+                slide.setNode(slider);
+                slide.setToX(0);
+                slide.play();
+                slider.setTranslateX(-176);
+                menuOpen = true;
+            }
+            else{
+                TranslateTransition slide = new TranslateTransition();
+                slide.setDuration(Duration.seconds(0.4));
+                slide.setNode(slider);
+                slide.setToX(-176);
+                slide.play();
+                slider.setTranslateX(0);
+                menuOpen = false;
+            }
         }
+    }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        slider.setTranslateX(-176);
+        menuOpen = false;
+        // Populate table names
     }
 }
