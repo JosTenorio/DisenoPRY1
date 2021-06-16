3b4ff41ef6406325cc981b0d157b6d0705672542
Jose Pablo Munoz <jpmunoz@estudiantec.cr>
2021-06-16 12:57:43 -0600

    Added details for kitchen

--- Restaurant POS/src/Controller/Items/OrderDetailItemController.java
+++ Restaurant POS/src/Controller/Items/OrderDetailItemController.java
@@ -1,6 +1,7 @@
 
 package Controller.Items;
 
+import Model.Item;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
@@ -11,6 +12,23 @@ public class OrderDetailItemController {
     private Label name;
     @FXML
     private ImageView checkBox;
+    
+    public void setData(Item item){
+        String itemNames = "  " + item.name;
+        if (item.sideDishes != null){
+            for (String dish : item.sideDishes){
+                itemNames += "\n\t" + dish;
+            }
+        }
+        if (!"".equals(item.notes)){
+            itemNames += "\n\tNota: " + item.notes;
+        }
+        name.setText(itemNames);
+    }  
+    
+    public ImageView getCheckBox(){
+        return this.checkBox;
+    }
    
     
 }
--- Restaurant POS/src/Controller/Items/OrderItemController.java
+++ Restaurant POS/src/Controller/Items/OrderItemController.java
@@ -3,13 +3,20 @@ package Controller.Items;
 
 import Model.Item;
 import Model.Order;
+import java.io.IOException;
 import java.util.ArrayList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import org.javatuples.Pair;
 
 public class OrderItemController {
+    
+    private ArrayList<Pair<Integer,ImageView>> checkBoxes;
 
     @FXML
     private Label tableName;
@@ -23,7 +30,29 @@ public class OrderItemController {
     public void setData(Order order){
         this.tableName.setText(order.tableName);
         this.orderAge.setText(order.age); 
-        
+        setDetails(order.items);
+    }
+    
+    private void setDetails(ArrayList<Item> items){
+        itemContainer.getChildren().clear();
+        this.checkBoxes = new ArrayList<>();
+        for (int i = 0; i < items.size(); i++){
+            FXMLLoader loader = new FXMLLoader();
+            try{
+                loader.setLocation(getClass().getResource("/View/Items/OrderDetailItemView.fxml"));
+                HBox hbox = loader.load();
+                OrderDetailItemController itemController = loader.getController();
+                itemController.setData(items.get(i));
+                itemContainer.getChildren().add(hbox);
+                this.checkBoxes.add(new Pair(items.get(i).number, itemController.getCheckBox()));
+            } catch (IOException ex) {
+                System.err.println(ex);
+            }
+        }
+    }
+    
+    public ArrayList<Pair<Integer, ImageView>> getCheckBoxes(){
+        return checkBoxes;
     }
     
 }
