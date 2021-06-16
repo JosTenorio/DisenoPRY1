14a36734ca5e2d05255841590533c303b04a0692
Jose Pablo Munoz <jpmunoz@estudiantec.cr>
2021-06-15 21:54:27 -0600

    Added category buttons for inventory

--- Restaurant POS/src/Model/Ingredient.java
+++ Restaurant POS/src/Model/Ingredient.java
@@ -1,5 +1,5 @@
-package Model;
 
+package Model;
 
 public class Ingredient {
     
@@ -7,15 +7,15 @@ public class Ingredient {
     public String description;
     public int minimum;
     public int quantity;
-    public String measurement;
+    public String unit;
     public String imgPath;
     
-    public Ingredient (String name, String description, int minimum, int quantity, String measurement, String imgPath) {
+    public Ingredient (String name, String description, int minimum, int quantity, String unit, String imgPath) {
         this.name = name;
         this.description = description;
         this.minimum = minimum;
         this.quantity = quantity;
-        this.measurement = measurement;
+        this.unit = unit;
         this.imgPath = imgPath;
     }
     
